fe88de991f2758cbfc5997a10a20804001c6adde
Danial <danial-baizak@mail.ru>
2023-07-04 01:07:53 +0600

    backend finished contacts and words

--- app/auth/router/router_add_word.py
+++ app/auth/router/router_add_word.py
@@ -1,6 +1,8 @@
 from fastapi import Depends, status, Response
 
 from app.utils import AppModel
+from ..models import Word
+from datetime import datetime
 
 from ..adapters.jwt_service import JWTData
 from ..service import Service, get_service
@@ -18,5 +20,7 @@ def add_word(
     svc: Service = Depends(get_service),
     jwt_data: JWTData = Depends(parse_jwt_user_data),
 ):
-    svc.word_repository.add_new_word(jwt_data.user_id, input.word)
+    timestamp = datetime.now()
+    word = Word(word=input.word, timestamp=timestamp)
+    svc.word_repository.add_new_word(jwt_data.user_id, word)
     return Response(status_code=status.HTTP_201_CREATED)
