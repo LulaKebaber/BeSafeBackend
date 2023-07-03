6d0fdaf7df1369f73bef76afcf29e997bbb13eed
Danial <danial-baizak@mail.ru>
2023-07-03 23:33:40 +0600

    add contact

---
+++ app/auth/router/router_add_contact.py
@@ -0,0 +1,24 @@
+from fastapi import Depends, status, Response
+
+from app.utils import AppModel
+
+from ..adapters.jwt_service import JWTData
+from ..service import Service, get_service
+from . import router
+from .dependencies import parse_jwt_user_data
+
+
+class AddContactRequest(AppModel):
+    name: str
+    phone: str
+    gps: bool
+
+
+@router.post("/users/words", status_code=status.HTTP_201_CREATED)
+def add_word(
+    input: AddContactRequest,
+    svc: Service = Depends(get_service),
+    jwt_data: JWTData = Depends(parse_jwt_user_data),
+):
+    svc.word_repository.add_new_contact(jwt_data.user_id, input.dict)
+    return Response(status_code=status.HTTP_201_CREATED)
