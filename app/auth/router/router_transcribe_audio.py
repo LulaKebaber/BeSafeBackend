8891f0deb4d603a45f7958a9320c1277f0a0c307
Danial <danial-baizak@mail.ru>
2023-07-28 03:01:21 +0600

    (STABLE) request to whisperAI implemented

--- app/auth/router/router_transcribe_audio.py
+++ app/auth/router/router_transcribe_audio.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, status, File, UploadFile
+from fastapi import Depends, status, File, UploadFile, HTTPException
 from ..models import Transcription
 from datetime import datetime
 from ..adapters.jwt_service import JWTData
@@ -6,11 +6,7 @@ from ..service import Service, get_service
 from . import router
 from .dependencies import parse_jwt_user_data
 
-from openai import OpenAI
-from openai.api_resources import Audio
-
-# Create an instance of the OpenAI client
-openai = OpenAI("your_openai_api_key")
+import requests
 
 @router.post("/users/transcriptions", status_code=status.HTTP_201_CREATED)
 def transcribe_audio(
@@ -18,12 +14,31 @@ def transcribe_audio(
     svc: Service = Depends(get_service),
     jwt_data: JWTData = Depends(parse_jwt_user_data),
 ):
+    # Make sure we're at the start of the file
+    file.file.seek(0)
+
     # Transcribe the audio file using OpenAI's Whisper ASR API
-    audio = Audio.create(openai, file=file.file.read(), model="whisper-1")
-    transcription = audio['transcription']
+    response = requests.post(
+        "https://api.openai.com/v1/audio/transcriptions",
+        headers={
+            "Authorization": "Bearer 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",
+        },
+        files={"file": (file.filename, file.file)},  # Corrected here
+        data={"model": "whisper-1"},
+    )
+
+    # Check the status of the request
+    if response.status_code != 200:
+        print(f"Response status code: {response.status_code}")
+        print(f"Response data: {response.json()}")
+        raise HTTPException(status_code=400, detail="Transcription failed")
+
+    # Get the transcription from the response
+    data = response.json()
 
     # Create a new Transcription object and save it to the database
     timestamp = datetime.now()
+    transcription = data["text"]
     transcription = Transcription(transcription=transcription, timestamp=timestamp)
     svc.transcription_repository.add_new_transcription(jwt_data.user_id, transcription)
 
