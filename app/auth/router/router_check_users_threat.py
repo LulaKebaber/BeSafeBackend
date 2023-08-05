11a242313ce18649159f8aa7e4a71e04bf768902
Danial <danial-baizak@mail.ru>
2023-08-05 16:25:07 +0600

    transcribing and threats

--- app/auth/router/router_check_users_threat.py
+++ app/auth/router/router_check_users_threat.py
@@ -1,31 +1,32 @@
-from fastapi import Depends, status, File, UploadFile, HTTPException, FastAPI
-from ..models import Transcription
-from datetime import datetime
+from fastapi import Depends, status
+from app.utils import AppModel
+from typing import List, Dict, Any
+
 from ..adapters.jwt_service import JWTData
-from ..service import Service, get_service, check_words_in_text
+from ..service import Service, get_service
 from . import router
 from .dependencies import parse_jwt_user_data
 
-@router.post("/users/contacts", status_code=status.HTTP_201_CREATED)
+class GetUsernameResponse(AppModel):
+    username: str
+    threat_recognised: bool
+    latitude: float
+    longitude: float
+
+class GetAllContactsResponse(AppModel):
+    threats: List[Dict[str, Any]]
+
+@router.get("/users/threats", status_code=status.HTTP_201_CREATED)
 def check_for_threat(
     svc: Service = Depends(get_service),
     jwt_data: JWTData = Depends(parse_jwt_user_data),
 ):
     contacts = svc.word_repository.get_user_contacts(user_id=jwt_data.user_id)
+    threat_users = []
 
     for contact in contacts:
         user = svc.repository.get_user_by_username(username=contact["username"])
-
         if user["threat_recognised"]:
-            return user["username"]
-    
-    
-    
-        # contacts = svc.word_repository.get_user_contacts(jwt_data.user_id)
-        # for contact in contacts:
-        #     # Предположим, что поле `username` указывает на имя пользователя для каждого контакта
-        #     # и что это уникальное значение для каждого пользователя
-        #     contact_username = contact["username"]
-        #     user = svc.repository.get_user_by_username(username=contact_username)
-        #     if user["threat_recognised"] == True:
-        #         return 
+            threat_users.append( )
+
+    return GetAllContactsResponse(threats=threat_users)
